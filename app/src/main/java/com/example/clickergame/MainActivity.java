e7cac702dc3fa5dc6a274dc6542dc5201067a2d0
91112021 <dalebolido@gmail.com>
2024-01-17 12:46:17 -0800

    Button now updates TextView

--- app/src/main/java/com/example/clickergame/MainActivity.java
+++ app/src/main/java/com/example/clickergame/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int money = 0;
@@ -14,12 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView green = (ImageView) findViewById(R.id.greenImg);
+        TextView moneyDisplay = (TextView) findViewById(R.id.moneyView);
 
         green.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
                 money++;
+                moneyDisplay.setText("You have " + money + "$");
             }
         });
 
